1b5507c84a8a25be09954755fd67e2cad7194a1e
xvzwhy <whosbuddyfr@gmail.com>
2026-07-18 18:36:22 -0400

    murdermystery.lua

--- murdermystery.lua
+++ murdermystery.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImM2OWVlNjM3LTQyMmMtNDdmMy1iYjhiLWNjN2E5MTAwNzNhNSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJuZXd1aSJ9fQ"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/iQNrLM5Grn/0"))()
